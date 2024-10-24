ef139a418583ce4f8f8dad34350c9e70f82bacb3
Daisuki Yukinoshita Yukino <1475215798@qq.com>
2024-10-24 20:55:50 +0800

    Update mt6985.mk

--- mt6985.mk
+++ mt6985.mk
@@ -1,1 +1,8 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
+# Inherit generic_ramdisk product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
